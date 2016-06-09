ec41d5e31c38ec36764f3f60df619b9190818729
R. Yin <916280067@qq.com>
2016-06-10 01:48:06 +0800

    ADD:cutwords init

---
+++ core/preprocess.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import time
+import random
+import string
+from flask import Blueprint, request, redirect, current_app, url_for, send_from_directory, json, abort, render_template, abort, make_response, session
+
+
+preprocess = Blueprint('preprocess', __name__, url_prefix='/preprocess')
+
+#@api.route('/team/icon/<int:team_id>')
+
+@preprocess.route('/cutwords/zh')
+def cutWords():
+	return 'error'
