5813347e9a29c60cfc01b0f61815f96c1b3ee99b
R. Yin <916280067@qq.com>
2016-07-09 20:06:39 +0800

    ADD：travel

--- module/travelrec.py
+++ module/travelrec.py
@@ -18,4 +18,5 @@ def travelrectest():
     travelList = [u"鸟巢",u"奥林匹克公园",u"北海公园",u"长安街",u"王府井大街",
                     u"颐和园",u"清华大学",u"南锣鼓巷",u"国子监",u"八达岭"]
     b = BeijingRounte()
+    print b
     return b.route(travelList)
